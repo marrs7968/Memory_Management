b9b69f790379ee3aaf811e798dff3dd63b6c9053
Sriram Ranganathan <sr7968@nyu.edu>
2026-06-09 21:53:24 -0400

    completed first fit algorithm logic successfully

--- memory.c
+++ memory.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include "oslabs.h"
 
-#define NULLBLOCK (struct MEMORY_BLOCK){0, 0, 0, 0};
+#define NULLBLOCK (struct MEMORY_BLOCK){0, 0, 0, 0}
 
 // Insert helper function from Google AI
 void insert_at(struct MEMORY_BLOCK *arr, int *size, int target_index, struct MEMORY_BLOCK block) {
@@ -23,27 +23,46 @@ struct MEMORY_BLOCK first_fit_allocate(int request_size, struct MEMORY_BLOCK mem
         return invalid_block;
     }
     for (int i = 0; i < *map_cnt; i++) {
-        if (memory_map[i].process_id == 0) {
+        if (memory_map[i].process_id == 0 && memory_map[i].segment_size >= request_size) {
             if (memory_map[i].segment_size == request_size) {
                 memory_map[i].process_id = process_id;
                 return memory_map[i];
             } else if (memory_map[i].segment_size > request_size) {
-                int alloc_start = &memory_map[i].start_address;
+                int alloc_start = memory_map[i].start_address;
                 int alloc_end = alloc_start + request_size - 1;
 
-                // CONTINUE HERE
                 int free_size = memory_map[i].segment_size - request_size;
                 int free_start = alloc_end + 1;
-                int free_end = free_start + free_size;
+                int free_end = free_start + free_size - 1;
 
                 struct MEMORY_BLOCK allocated_block = {alloc_start, alloc_end, request_size, process_id};
                 struct MEMORY_BLOCK free_block = {free_start, free_end, free_size, 0};
                 
                 insert_at(memory_map, map_cnt, i, allocated_block);
                 insert_at(memory_map, map_cnt, i + 1, free_block);
+                memmove(&memory_map[i + 2], &memory_map[i + 3], (*map_cnt - (i + 3)) * sizeof(struct MEMORY_BLOCK));
+                (*map_cnt)--;
+
+                return allocated_block;
             }
-        } else {
-            return NULLBLOCK;
         }
     }
+    return NULLBLOCK;
+}
+
+
+struct MEMORY_BLOCK best_fit_allocate(int request_size, struct MEMORY_BLOCK memory_map[MAPMAX], int *map_cnt, int process_id) {
+    return NULLBLOCK;
+}
+
+struct MEMORY_BLOCK worst_fit_allocate(int request_size, struct MEMORY_BLOCK memory_map[MAPMAX], int *map_cnt, int process_id) {
+    return NULLBLOCK;
+}
+
+struct MEMORY_BLOCK next_fit_allocate(int request_size, struct MEMORY_BLOCK memory_map[MAPMAX], int *map_cnt, int process_id, int last_address) {
+    return NULLBLOCK;
+}
+
+void release_memory(struct MEMORY_BLOCK freed_block, struct MEMORY_BLOCK memory_map[MAPMAX], int *map_cnt) {
+    return;
 }
