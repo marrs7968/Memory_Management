1795c2317c7ee59e5ce3cc55a87a5725ddf2abd8
Srira <sr7968@nyu.edu>
2026-06-11 16:52:55 -0400

    successfully implemented worst fit algorithm; used best fit as template with simple change to largest block size rather than smallest difference

--- memory.c
+++ memory.c
@@ -96,11 +96,85 @@ struct MEMORY_BLOCK first_fit_allocate(int request_size, struct MEMORY_BLOCK mem
     return NULLBLOCK;
 }
 
+// ============= WORST FIT =============
 struct MEMORY_BLOCK worst_fit_allocate(int request_size, struct MEMORY_BLOCK memory_map[MAPMAX], int *map_cnt, int process_id) {
+    if (request_size <= 0) {
+        struct MEMORY_BLOCK invalid_block = {-1, -1, -1, -1};
+        return invalid_block;
+    }
+    int largest_size = INT_MIN;
+    int largest_size_index = 0;
+    for (int i = 0; i < *map_cnt; i++) {
+        if (memory_map[i].process_id == 0 && memory_map[i].segment_size >= request_size) {
+            if (memory_map[i].segment_size == request_size) {
+                memory_map[i].process_id = process_id;
+                return memory_map[i];
+            } else if (memory_map[i].segment_size > request_size) {
+                if (memory_map[i].segment_size > largest_size) {
+                    largest_size = memory_map[i].segment_size;
+                    largest_size_index = i;
+                    continue;
+                }
+            }
+        }
+    }
+    if (largest_size > INT_MIN) {
+        int alloc_start = memory_map[largest_size_index].start_address;
+        int alloc_end = alloc_start + request_size - 1;
+
+        int free_size = memory_map[largest_size_index].segment_size - request_size;
+        int free_start = alloc_end + 1;
+        int free_end = free_start + free_size - 1;
+
+        struct MEMORY_BLOCK allocated_block = {alloc_start, alloc_end, request_size, process_id};
+        struct MEMORY_BLOCK free_block = {free_start, free_end, free_size, 0};
+        
+        insert_at(memory_map, map_cnt, largest_size_index, allocated_block);
+        insert_at(memory_map, map_cnt, largest_size_index + 1, free_block);
+        // accounting for the original free block, shifting elements left and decrementing
+        memmove(&memory_map[largest_size_index + 2], &memory_map[largest_size_index + 3], (*map_cnt - (largest_size_index + 3)) * sizeof(struct MEMORY_BLOCK));
+        (*map_cnt)--;
+
+        return allocated_block;
+    }
     return NULLBLOCK;
 }
 
+// ============= NEXT FIT =============
 struct MEMORY_BLOCK next_fit_allocate(int request_size, struct MEMORY_BLOCK memory_map[MAPMAX], int *map_cnt, int process_id, int last_address) {
+    if (request_size <= 0) {
+        struct MEMORY_BLOCK invalid_block = {-1, -1, -1, -1};
+        return invalid_block;
+    }
+    for (int i = 0; i < *map_cnt; i++) {
+        if (memory_map[i].process_id == 0 && memory_map[i].segment_size >= request_size) {
+            int last_allocated_size = (memory_map[i].start_address == last_address) ? memory_map[i].segment_size : INT_MAX;
+            if ((memory_map[i].start_address > last_address) && memory_map[i].segment_size >= last_allocated_size) {
+                if (memory_map[i].segment_size == request_size) {
+                    memory_map[i].process_id = process_id;
+                    return memory_map[i];
+                } else if (memory_map[i].segment_size > request_size) {
+                    int alloc_start = memory_map[i].start_address;
+                    int alloc_end = alloc_start + request_size - 1;
+
+                    int free_size = memory_map[i].segment_size - request_size;
+                    int free_start = alloc_end + 1;
+                    int free_end = free_start + free_size - 1;
+
+                    struct MEMORY_BLOCK allocated_block = {alloc_start, alloc_end, request_size, process_id};
+                    struct MEMORY_BLOCK free_block = {free_start, free_end, free_size, 0};
+                    
+                    insert_at(memory_map, map_cnt, i, allocated_block);
+                    insert_at(memory_map, map_cnt, i + 1, free_block);
+                    // accounting for the original free block, shifting elements left and decrementing
+                    memmove(&memory_map[i + 2], &memory_map[i + 3], (*map_cnt - (i + 3)) * sizeof(struct MEMORY_BLOCK));
+                    (*map_cnt)--;
+
+                    return allocated_block;
+                }
+            }
+        }
+    }
     return NULLBLOCK;
 }
 
