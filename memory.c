cc4d4375c36e213d52274a5fbc8e8c23fe98383e
Srira <sr7968@nyu.edu>
2026-06-11 16:12:21 -0400

    successfully implemented best fit algorithm; logic involved iterating through full memory map, tracking memory block with smallest size difference and corresponding index, then splitting and allocating block at index after iteration is complete

--- memory.c
+++ memory.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <limits.h>
 #include <string.h>
 #include "oslabs.h"
 
 #define NULLBLOCK (struct MEMORY_BLOCK){0, 0, 0, 0}
 
-// Insert helper function from Google AI
+// Insert helper function adapted from Google AI
 void insert_at(struct MEMORY_BLOCK *arr, int *size, int target_index, struct MEMORY_BLOCK block) {
     // Shift elements using memmove
     // Destination: target + 1 index, Source: target index
@@ -14,6 +15,50 @@ void insert_at(struct MEMORY_BLOCK *arr, int *size, int target_index, struct MEM
     
     arr[target_index] = block;
     (*size)++;
+}
+
+// ============= BEST FIT =============
+struct MEMORY_BLOCK best_fit_allocate(int request_size, struct MEMORY_BLOCK memory_map[MAPMAX], int *map_cnt, int process_id) {
+    if (request_size <= 0) {
+        struct MEMORY_BLOCK invalid_block = {-1, -1, -1, -1};
+        return invalid_block;
+    }
+    int closest_size_diff = INT_MAX;
+    int closest_size_index = 0;
+    for (int i = 0; i < *map_cnt; i++) {
+        if (memory_map[i].process_id == 0 && memory_map[i].segment_size >= request_size) {
+            if (memory_map[i].segment_size == request_size) {
+                memory_map[i].process_id = process_id;
+                return memory_map[i];
+            } else if (memory_map[i].segment_size > request_size) {
+                if ((memory_map[i].segment_size - request_size) < closest_size_diff) {
+                    closest_size_diff = memory_map[i].segment_size - request_size;
+                    closest_size_index = i;
+                    continue;
+                }
+            }
+        }
+    }
+    if (closest_size_diff < INT_MAX) {
+        int alloc_start = memory_map[closest_size_index].start_address;
+        int alloc_end = alloc_start + request_size - 1;
+
+        int free_size = memory_map[closest_size_index].segment_size - request_size;
+        int free_start = alloc_end + 1;
+        int free_end = free_start + free_size - 1;
+
+        struct MEMORY_BLOCK allocated_block = {alloc_start, alloc_end, request_size, process_id};
+        struct MEMORY_BLOCK free_block = {free_start, free_end, free_size, 0};
+        
+        insert_at(memory_map, map_cnt, closest_size_index, allocated_block);
+        insert_at(memory_map, map_cnt, closest_size_index + 1, free_block);
+        // accounting for the original free block, shifting elements left and decrementing
+        memmove(&memory_map[closest_size_index + 2], &memory_map[closest_size_index + 3], (*map_cnt - (closest_size_index + 3)) * sizeof(struct MEMORY_BLOCK));
+        (*map_cnt)--;
+
+        return allocated_block;
+    }
+    return NULLBLOCK;
 }
 
 // ============= FIRST FIT =============
@@ -40,6 +85,7 @@ struct MEMORY_BLOCK first_fit_allocate(int request_size, struct MEMORY_BLOCK mem
                 
                 insert_at(memory_map, map_cnt, i, allocated_block);
                 insert_at(memory_map, map_cnt, i + 1, free_block);
+                // accounting for the original free block, shifting elements left and decrementing
                 memmove(&memory_map[i + 2], &memory_map[i + 3], (*map_cnt - (i + 3)) * sizeof(struct MEMORY_BLOCK));
                 (*map_cnt)--;
 
@@ -47,11 +93,6 @@ struct MEMORY_BLOCK first_fit_allocate(int request_size, struct MEMORY_BLOCK mem
             }
         }
     }
-    return NULLBLOCK;
-}
-
-
-struct MEMORY_BLOCK best_fit_allocate(int request_size, struct MEMORY_BLOCK memory_map[MAPMAX], int *map_cnt, int process_id) {
     return NULLBLOCK;
 }
 
@@ -64,5 +105,4 @@ struct MEMORY_BLOCK next_fit_allocate(int request_size, struct MEMORY_BLOCK memo
 }
 
 void release_memory(struct MEMORY_BLOCK freed_block, struct MEMORY_BLOCK memory_map[MAPMAX], int *map_cnt) {
-    return;
 }
