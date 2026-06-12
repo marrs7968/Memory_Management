ef84a469325d6b8c9b25adf2ee9316e844f1eb5b
Srira <sr7968@nyu.edu>
2026-06-12 18:56:27 -0400

    adjusted next fit algorithm function to correct logic

--- memory.c
+++ memory.c
@@ -147,31 +147,30 @@ struct MEMORY_BLOCK next_fit_allocate(int request_size, struct MEMORY_BLOCK memo
         return invalid_block;
     }
     for (int i = 0; i < *map_cnt; i++) {
-        if (memory_map[i].process_id == 0 && memory_map[i].segment_size >= request_size) {
-            int last_allocated_size = (memory_map[i].start_address == last_address) ? memory_map[i].segment_size : INT_MAX;
-            if ((memory_map[i].start_address > last_address) && memory_map[i].segment_size >= last_allocated_size) {
-                if (memory_map[i].segment_size == request_size) {
-                    memory_map[i].process_id = process_id;
-                    return memory_map[i];
-                } else if (memory_map[i].segment_size > request_size) {
-                    int alloc_start = memory_map[i].start_address;
-                    int alloc_end = alloc_start + request_size - 1;
+        if (memory_map[i].process_id == 0 
+            && memory_map[i].segment_size >= request_size
+            && memory_map[i].start_address >= last_address) {
+            if (memory_map[i].segment_size == request_size) {
+                memory_map[i].process_id = process_id;
+                return memory_map[i];
+            } else if (memory_map[i].segment_size > request_size) {
+                int alloc_start = memory_map[i].start_address;
+                int alloc_end = alloc_start + request_size - 1;
 
-                    int free_size = memory_map[i].segment_size - request_size;
-                    int free_start = alloc_end + 1;
-                    int free_end = free_start + free_size - 1;
+                int free_size = memory_map[i].segment_size - request_size;
+                int free_start = alloc_end + 1;
+                int free_end = free_start + free_size - 1;
 
-                    struct MEMORY_BLOCK allocated_block = {alloc_start, alloc_end, request_size, process_id};
-                    struct MEMORY_BLOCK free_block = {free_start, free_end, free_size, 0};
-                    
-                    insert_at(memory_map, map_cnt, i, allocated_block);
-                    insert_at(memory_map, map_cnt, i + 1, free_block);
-                    // accounting for the original free block, shifting elements left and decrementing
-                    memmove(&memory_map[i + 2], &memory_map[i + 3], (*map_cnt - (i + 3)) * sizeof(struct MEMORY_BLOCK));
-                    (*map_cnt)--;
+                struct MEMORY_BLOCK allocated_block = {alloc_start, alloc_end, request_size, process_id};
+                struct MEMORY_BLOCK free_block = {free_start, free_end, free_size, 0};
+                
+                insert_at(memory_map, map_cnt, i, allocated_block);
+                insert_at(memory_map, map_cnt, i + 1, free_block);
+                // accounting for the original free block, shifting elements left and decrementing
+                memmove(&memory_map[i + 2], &memory_map[i + 3], (*map_cnt - (i + 3)) * sizeof(struct MEMORY_BLOCK));
+                (*map_cnt)--;
 
-                    return allocated_block;
-                }
+                return allocated_block;
             }
         }
     }
